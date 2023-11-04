ee768fed675a5ae1d2d9df1eea89b7ac4bbd8503
Atai_Cholponkulov <ataicholponkulov@gmail.com>
2023-11-05 01:23:16 +0600

    v1.0 - checkstyle

--- ewm-service/src/main/java/ru/practicum/ewm/service/exception/model/BaseException.java
+++ ewm-service/src/main/java/ru/practicum/ewm/service/exception/model/BaseException.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 
 @Getter
 public class BaseException extends RuntimeException {
+
     protected final String reason;
+
     protected BaseException(String message, String reason) {
         super(message);
         this.reason = reason;
